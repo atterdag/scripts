9698550e94ef989d4797b3626be7b125908a5e1f
Valdemar Lemche <valdemar@lemche.net>
2017-10-02 14:31:20 +0200

    Added some more steps to also install docker, and ensure that it works behind a forwarding proxy server

--- docker/00-configure-docker.sh
+++ docker/00-configure-docker.sh
@@ -1,10 +1,68 @@
 #!/bin/sh
 echo '***'
-echo '*** copy certificates to docker configuration directory '
+echo '*** removing any past versions of docker'
 echo '***'
-sudo cp /net/main/srv/common-setup/ssl/cacert.pem /etc/docker/certs/ca.crt
-sudo cp /net/main/srv/common-setup/ssl/docker.example.com-cert.pem /etc/docker/certs/docker.crt
-sudo cp /net/main/srv/common-setup/ssl/docker.example.com-key.pem /etc/docker/certs/docker.key
+apt-get --yes remove docker docker-engine docker.io
+
+echo '***'
+echo '*** updating APT repositories'
+echo '***'
+apt-get update
+apt-get -y install \
+     apt-transport-https \
+     ca-certificates \
+     gnupg2 \
+     software-properties-common \
+     wget
+
+echo '***'
+echo '*** adding docker repository GPG key'
+echo '***'
+wget -q -O - https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+echo '***'
+echo '*** check that GPG key have been registered'
+echo '***'
+apt-key fingerprint 0EBFCD88
+
+echo '***'
+echo '*** adding docker APT repository'
+echo '***'
+cat > /etc/apt/sources.list.d/docker.list << EOF
+deb [arch=amd64] https://download.docker.com/linux/debian stretch stable
+# deb-src [arch=amd64] https://download.docker.com/linux/debian stretch stable
+EOF
+
+echo '***'
+echo '*** updating APT repositories'
+echo '***'
+apt-get update
+
+echo '***'
+echo '*** installing docker-ce'
+echo '***'
+apt-get -y install docker-ce
+
+echo '***'
+echo '*** adding forwarding proxy configuration'
+echo '***'
+if [ ! -d /etc/systemd/system/docker.service.d ]; do mkdir -p /etc/systemd/system/docker.service.d; fi
+cat > /etc/systemd/system/docker.service.d/http-proxy.conf << EOF
+[Service]
+Environment="HTTP_PROXY=http://cache.example.com:3128/"
+Environment="HTTPS_PROXY=http://cache.example.com:3128/"
+Environment="FTP_PROXY=http://cache.example.com:3128/"
+Environment="NO_PROXY=localhost,127.0.0.1,LocalAddress,example.com,example.lan"
+EOF
+systemctl daemon-reload
+
+echo '***'
+echo '*** copy certificates to docker configuration directory'
+echo '***'
+if [ ! -d /etc/docker/certs ]; do mkdir -p /etc/docker/certs/; fi
+sudo cp /net/main/srv/common-setup/ssl/cacert.pem /etc/ssl/cacert.pem
+sudo cp /net/main/srv/common-setup/ssl/${HOSTNAME}.example.com-cert.pem /etc/ssl/${HOSTNAME}.example.com-cert.pem
+sudo cp /net/main/srv/common-setup/ssl/${HOSTNAME}.example.com-key.pem /etc/ssl/private/${HOSTNAME}.example.com-key.pem
 
 echo '***'
 echo '*** creating docker daemon configuration '
@@ -15,10 +73,10 @@ cat > /etc/docker/daemon.json << EOF
     "insecure-registries": ["registry.example.com:5000"],
     "tls": true,
     "tlsverify": true,
-    "tlscacert": "/etc/docker/certs/ca.crt",
-    "tlscert": "/etc/docker/certs/docker.crt",
-    "tlskey": "/etc/docker/certs/docker.key",
-    "debug": true
+    "tlscacert": "/etc/ssl/cacert.pem",
+    "tlscert": "/etc/ssl/${HOSTNAME}.example.com-cert.pem",
+    "tlskey": "/etc/ssl/private/${HOSTNAME}.example.com-key.pem",
+    "debug": false
 }
 EOF
 
@@ -26,3 +84,8 @@ echo '***'
 echo '*** restarting docker daemon'
 echo '***'
 /etc/init.d/docker restart
+
+echo '***'
+echo '*** checking that docker works'
+echo '***'
+docker run hello-world
