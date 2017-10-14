51ab36728204d40e47da426b5418d525482fa204
Valdemar Lemche <valdemar@lemche.net>
2017-10-14 08:10:21 +0200

    Split full synchronize commands from syncActiveNode commands into two scripts

--- was/start_full_synchronize_all_nodes.py
+++ was/start_full_synchronize_all_nodes.py
@@ -9,17 +9,3 @@ for node in nodes:
     result = AdminControl.invoke(nodeCompleteObjectName, 'sync')
     result = AdminControl.invoke(nodeAgentCompleteObjectName, 'refreshRepositoryEpoch')
     result = AdminControl.invoke(cellCompleteobjectName, 'syncNode', '[' + node + ']')
-
-print '##############################################################################'
-print '# Synchronizing configuration                                                #'
-print '##############################################################################'
-print
-dmgr = AdminControl.completeObjectName('type=DeploymentManager,*')
-nodes = AdminControl.invoke(dmgr, 'syncActiveNodes', 'true')
-print 'the following nodes have been synchronized:'
-for node in nodes.splitlines():
-  print ' - ' + node
-
-print
-print '***** saving configuration *****'
-result = AdminConfig.save()
