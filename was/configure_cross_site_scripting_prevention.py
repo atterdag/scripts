6b4af4145a710d24ec0206d31ad26752208abd90
Valdemar Lemche <valdemar@lemche.net>
2017-10-15 15:30:01 +0200

    Renamed function httponlyCookies to setHttponlyCookies to be more telling. Replaced hardcoded reference to common.py in execfile with logic that determines the full path to common.py from the script being called by wsadmin

--- was/configure_cross_site_scripting_prevention.py
+++ was/configure_cross_site_scripting_prevention.py
@@ -1,13 +1,18 @@
-jsessionIdCookieName='JSESSIONID'
+import os, re, java.io.File
+command = os.environ.get('IBM_JAVA_COMMAND_LINE')
+for arg in command.split(' -'):
+  if re.match('^f\s',arg):
+    script_directory = java.io.File(arg.split()[1]).getParent()
+    execfile( script_directory + '/common.py')
 
-execfile('common.py')
+jsessionIdCookieName='JSESSIONID'
 
 nodes = AdminConfig.list('Node').splitlines()
 for node in nodes:
   nodeName = AdminConfig.showAttribute(node, 'name')
   servers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName ' + nodeName + ']').splitlines()
   for server in servers:
-    httponlyCookies(server,jsessionIdCookieName)
+    setHttponlyCookies(server,jsessionIdCookieName)
 
 print "Setting httponly for all cookies custom property for Global Security"
 result = AdminTask.setAdminActiveSecuritySettings('[-customProperties[\"com.ibm.ws.security.addHttpOnlyAttributeToCookies=true\"]]')
