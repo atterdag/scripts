88ffe5eabb31f35f1e572a47be882a56d60e69d6
atterdag <valdemar@lemche.net>
2017-10-23 22:22:07 +0200

    Created script to quickly create a new vm clone

---
+++ bin/clone-vm.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+if [ "$2" == "" ]; then
+    echo "usage: $0 <name of source vm> <name of target vm>"
+    exit 1
+fi
+vmrun -T ws clone /media/vmware/${1}/${1}.vmx /media/vmware/${2}/${2}.vmx full -cloneName $2
