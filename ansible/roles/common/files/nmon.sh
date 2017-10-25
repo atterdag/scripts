c5b1376792e4d97421e7913c4df32a312a5c6900
Valdemar Lemche <valdemar@lemche.net>
2017-10-26 00:15:49 +0200

    Remove "..." from yaml files Changed become: true to become: yes to be more consistent added autofs package, and configuration to common role added monitoring packages to common role added ufw to common role added open-vm-tools packages to common role if host is vmware added sysdig syscall logger to common role added ufw rule to docker role prepared to k8sconsole role added proxy environment to kubeadm, and kubectl commands

---
+++ ansible/roles/common/files/nmon.sh
@@ -0,0 +1,12 @@
+# This starts monitors:
+#  c = CPU by processor
+#  m = Memory & Swap stats
+#  d = Disk I/O Graphs
+#  l = longer term CPU averages
+#  a = Disk Adapter
+#  n = Network stats
+#  t = Top Process Stats
+export NMON=cmdlant
+
+# This alias is for PuTTY users that want to see lines rather than just lqqk
+alias nmon="TERM=linux nmon"
