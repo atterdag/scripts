5954e915c5d16d3d8c0e656368112048cc9e39d1
Valdemar Lemche <valdemar@lemche.net>
2017-11-16 09:34:38 +0100

    added cluster testing scripts

---
+++ wsadmin/verfication_test_cleanup.py
@@ -0,0 +1,51 @@
+import os, re, java.io.File
+command = os.environ.get('IBM_JAVA_COMMAND_LINE')
+for arg in command.split(' -'):
+  if re.match('^f\s',arg):
+    script_directory = java.io.File(arg.split()[1]).getParent()
+    execfile( script_directory + '/common.py')
+
+cell = AdminControl.getCell()
+
+managedNodeNames = AdminTask.listManagedNodes().splitlines()
+for managedNodeName in managedNodeNames:
+  applicationServers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName ' + managedNodeName + ']').splitlines()
+  for applicationServer in applicationServers:
+    applicationServerName = AdminConfig.showAttribute(applicationServer, 'name')
+    if applicationServerName == 'testserver_' + managedNodeName:
+      print 'stopping DefaultApplication.ear on ' + managedNodeName
+      appManager = AdminControl.queryNames('type=ApplicationManager,process=testserver_' + managedNodeName + ',*')
+      result = AdminControl.invoke(appManager, 'stopApplication','DefaultApplication.ear')
+
+apps = AdminApp.list().splitlines()
+for app in apps:
+  if ( app == 'DefaultApplication.ear' ):
+    print 'uninstalling DefaultApplication.ear'
+    result =  AdminApp.uninstall('DefaultApplication.ear')
+
+saveConfiguration()
+
+managedNodeNames = AdminTask.listManagedNodes().splitlines()
+for managedNodeName in managedNodeNames:
+  applicationServers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName ' + managedNodeName + ']').splitlines()
+  for applicationServer in applicationServers:
+    applicationServerName = AdminConfig.showAttribute(applicationServer, 'name')
+    if applicationServerName == 'testserver_' + managedNodeName:
+      print 'stopping ' + applicationServerName + ' on ' + managedNodeName
+      result = AdminControl.stopServer(applicationServerName,managedNodeName)
+      print 'deleting ' + applicationServerName + ' on ' + managedNodeName
+      result = AdminTask.deleteServer('[-serverName ' + applicationServerName + ' -nodeName ' + managedNodeName + ' ]')
+
+saveConfiguration()
+
+clusters = AdminConfig.list('ServerCluster').splitlines()
+for cluster in clusters:
+  clusterName = AdminConfig.showAttribute(cluster,'name')
+  if ( clusterName == 'TestCluster' ):
+    print 'deleting TestCluster'
+    result = AdminTask.deleteCluster('[-clusterName TestCluster ]')
+
+synchronizeActiveNodes()
+
+print ''
+print '>>>>>>>>> The test cluster, servers etc have now been removed'
---
+++ wsadmin/verfication_test_testing.py
@@ -0,0 +1,38 @@
+import os, re, java.io.File, sys
+command = os.environ.get('IBM_JAVA_COMMAND_LINE')
+for arg in command.split(' -'):
+  if re.match('^f\s',arg):
+    script_directory = java.io.File(arg.split()[1]).getParent()
+    execfile( script_directory + '/common.py')
+
+cell = AdminControl.getCell()
+
+print 'check that you can login to the following URLs:'
+unmanagedNodeNames = AdminTask.listUnmanagedNodes().splitlines()
+for unmanagedNodeName in unmanagedNodeNames:
+  unmanagedNode = AdminConfig.getid('/Cell:' + cell + '/Node:' + unmanagedNodeName + ' ')
+  unmanagedNodeNameHostName = AdminConfig.showAttribute(unmanagedNode, 'hostName')
+  print ' https://' + unmanagedNodeNameHostName + '/snoop'
+
+print 'after checking that snoop works, then *DO NOT* close your browser.'
+print 'press [ENTER] when you are done'
+result = sys.stdin.readline()
+
+print 'testing session failover'
+managedNodeNames = AdminTask.listManagedNodes().splitlines()
+for managedNodeName in managedNodeNames:
+  applicationServers = AdminTask.listServers('[-serverType APPLICATION_SERVER -nodeName ' + managedNodeName + ']').splitlines()
+  for applicationServer in applicationServers:
+    applicationServerName = AdminConfig.showAttribute(applicationServer, 'name')
+    if applicationServerName == 'testserver_' + managedNodeName:
+      print 'stopping ' + applicationServerName + ' on ' + managedNodeName
+      result = AdminControl.stopServer(applicationServerName,managedNodeName)
+      print ''
+      print '>>>>>>>>>> check that snoop is still working on the webserver(s) listed above, and that you are not asked to login again'
+      print 'press [ENTER] when you are done'
+      result = sys.stdin.readline()
+      print 'starting ' + applicationServerName + ' on ' + managedNodeName
+      result = AdminControl.startServer(applicationServerName,managedNodeName)
+
+print ''
+print '>>>>>>>>> So if you did not lose you snoop session while starting, and stopping the servers, then that means that WAS is working as expected'
