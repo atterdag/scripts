ae6361289a694868c912407056ef4171249ffb00
Valdemar Lemche <valdemar@lemche.net>
2019-06-23 23:12:34 +0200

    cleaned up systemd services to start/stop

--- bin/install-openstack-12-typical-commands.sh
+++ bin/install-openstack-12-typical-commands.sh
@@ -7,18 +7,25 @@ sudo systemctl start \
   rabbitmq-server \
   memcached \
   etcd \
-  dirsrv@dir.service \
+  dirsrv-admin \
+  dirsrv@default.service \
   pki-tomcatd \
   apache2 \
+  ipvsadm \
+  haproxy \
   glance-registry \
   glance-api \
+  qemu-kvm \
   nova-api \
+  nova-xvpvncproxy \
+  nova-console \
   nova-consoleauth \
   nova-scheduler \
   nova-conductor \
   nova-novncproxy \
   nova-compute \
   neutron-server \
+  neutron-l3-agent \
   neutron-linuxbridge-agent \
   neutron-dhcp-agent \
   neutron-metadata-agent \
@@ -26,22 +33,20 @@ sudo systemctl start \
   cinder-scheduler \
   tgt \
   cinder-volume \
+  designate-worker \
+  designate-producer \
   designate-central \
   designate-api \
   designate-agent \
-  designate-mdns \
-  designate-pool-manager \
-  designate-sink \
-  designate-zone-manager
+  designate-mdns
 
 sudo systemctl stop \
-  designate-zone-manager \
-  designate-sink \
-  designate-pool-manager \
   designate-mdns \
   designate-agent \
   designate-api \
   designate-central \
+  designate-producer \
+  designate-worker \
   cinder-volume \
   tgt \
   cinder-scheduler \
@@ -49,18 +54,25 @@ sudo systemctl stop \
   neutron-metadata-agent \
   neutron-dhcp-agent \
   neutron-linuxbridge-agent \
+  neutron-l3-agent \
   neutron-server \
   nova-compute \
   nova-novncproxy \
   nova-conductor \
   nova-scheduler \
   nova-consoleauth \
+  nova-console \
+  nova-xvpvncproxy \
   nova-api \
+  qemu-kvm \
   glance-api \
   glance-registry \
+  haproxy \
+  ipvsadm \
   apache2 \
   pki-tomcatd \
-  dirsrv@dir.service \
+  dirsrv-admin \
+  dirsrv@default.service \
   etcd \
   memcached \
   rabbitmq-server \
