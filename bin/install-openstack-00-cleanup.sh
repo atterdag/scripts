a49ef0fec0c811a4b45f1244c863ec1d86776f42
Valdemar Lemche <valdemar@lemche.net>
2019-06-23 23:06:49 +0200

    added commands to revert to specific installed packages baseline

--- bin/install-openstack-00-cleanup.sh
+++ bin/install-openstack-00-cleanup.sh
@@ -108,3 +108,9 @@ sudo apt-get install --yes python-pip
 for i in $(pip list | awk '{print $1}'); do sudo -i pip uninstall -y $i; done
 sudo apt-get --reinstall --yes install \
   $(echo $(dpkg -l | awk '{print $2}' | tail -n +5 | grep ^python-) | sed 's|\n| |g')
+
+##############################################################################
+# Remove all packages installed since baseline
+##############################################################################
+sudo apt-get --purge --yes remove \
+  $(cat os_installed_packages.txt new_packages.txt | sort | uniq -u | tr '\n' ' ')
