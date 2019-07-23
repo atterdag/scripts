fdc61de1bcdf5d88b321508a87e588edd1d37002
Valdemar Lemche <valdemar@lemche.net>
2019-07-23 13:45:05 +0200

    made genpasswd func persistent

--- bin/install-openstack-01-configuration.sh
+++ bin/install-openstack-01-configuration.sh
@@ -8,11 +8,15 @@ sudo -i id
 ##############################################################################
 # Create function to generate passwords
 ##############################################################################
+cat << EOF | sudo tee /etc/profile.d/genpasswd.sh
 genpasswd() {
 	local l=$1
        	[ "$l" == "" ] && l=16
       	tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
 }
+EOF
+source /etc/profile.d/genpasswd.sh
+
 ##############################################################################
 # Create directory to store openstack files
 ##############################################################################
