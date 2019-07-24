61767c13f389b4ce91848b7d1d7385f9c7e5b691
Valdemar Lemche <valdemar@lemche.net>
2019-07-24 23:25:42 +0200

    forgot to backslash variables

--- bin/install-openstack-01-configuration.sh
+++ bin/install-openstack-01-configuration.sh
@@ -10,9 +10,9 @@ sudo -i id
 ##############################################################################
 cat << EOF | sudo tee /etc/profile.d/genpasswd.sh
 genpasswd() {
-	local l=$1
-       	[ "$l" == "" ] && l=16
-      	tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+	local l=\$1
+       	[ "\$l" == "" ] && l=16
+      	tr -dc A-Za-z0-9_ < /dev/urandom | head -c \${l} | xargs
 }
 EOF
 source /etc/profile.d/genpasswd.sh
