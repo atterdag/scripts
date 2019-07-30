d9f0c6cbfce385877d220d6fd518240204cefdd6
Valdemar Lemche <valdemar@lemche.net>
2019-07-30 17:37:05 +0200

    removed echo in front of commands to debug them because I ... forgot

--- openstack/500-openstack-services/565-designate-post.sh
+++ openstack/500-openstack-services/565-designate-post.sh
@@ -7,17 +7,17 @@ openstack dns service list
 ##############################################################################
 # Create DNS zones, and records for controller
 ##############################################################################
-echo openstack zone create \
+openstack zone create \
   --email hostmaster@${DNS_DOMAIN} \
   ${DNS_DOMAIN}.
-echo openstack zone create \
+openstack zone create \
   --email hostmaster@${DNS_DOMAIN} \
   ${DNS_REVERSE_DOMAIN}.
-echo openstack recordset create \
+openstack recordset create \
   --record "${CONTROLLER_IP_ADDRESS}" \
   --type A ${DNS_DOMAIN}. \
   $(echo $CONTROLLER_FQDN | awk -F'.' '{print $1}')
-echo openstack recordset create \
+openstack recordset create \
   --record "${CONTROLLER_FQDN}." \
   --type PTR ${DNS_REVERSE_DOMAIN}. \
   $(echo $CONTROLLER_IP_ADDRESS | awk -F'.' '{print $4}')
