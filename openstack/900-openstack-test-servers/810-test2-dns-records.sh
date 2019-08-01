8202b70f0fbd89d18c6c840238eafac02f746a50
Valdemar Lemche - DSV <Valdemar.Lemche@dsv.com>
2019-08-01 20:21:59 +0200

    corrected that I was creating dns records in the script that should create ports, and vice-versa.

--- openstack/900-openstack-test-servers/810-test2-dns-records.sh
+++ openstack/900-openstack-test-servers/810-test2-dns-records.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
 ##############################################################################
-# Create a fixed IP ports
+# Create DNZ records for test2
 ##############################################################################
-openstack port create \
-  --fixed-ip ip-address=192.168.1.130 \
-  --network default \
-  test2_inside
-openstack port create \
-  --fixed-ip ip-address=172.16.0.130 \
-  --network servers \
-  test2_servers
-openstack port create \
-  --fixed-ip ip-address=10.0.0.130 \
-  --network dmz \
-  test2_dmz
+openstack recordset create \
+  --record '192.168.1.130' \
+  --type A ${DNS_DOMAIN}. \
+  test2
+openstack recordset create \
+  --record 'test2.se.lemche.net.' \
+  --type PTR 1.168.192.in-addr.arpa. \
+  130
+
+openstack recordset create \
+  --record '172.16.0.130' \
+  --type A servers.${DNS_DOMAIN}. \
+  test2
+openstack recordset create \
+  --record 'test2.servers.se.lemche.net.' \
+  --type PTR 0.16.172.in-addr.arpa. \
+  130
+
+openstack recordset create \
+  --record '10.0.0.130' \
+  --type A dmz.${DNS_DOMAIN}. \
+  test2
+openstack recordset create \
+  --record 'test2.dmz.se.lemche.net.' \
+  --type PTR 0.0.10.in-addr.arpa. \
+  130
