6eb7954df8fa44c2412d132e728f45d57852e263
Valdemar Lemche <valdemar@lemche.net>
2019-08-01 14:36:25 +0200

    added separate volume template for both debian 9, debian 10, centos 18.04, and centos 7

---
+++ openstack/700-openstack-infrastructure/782-create-volume-template-centos-7.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##############################################################################
+# Create volume template on Controller host
+##############################################################################
+openstack volume create \
+  --description 'debian-10-openstack-amd64 template volume' \
+  --image debian-10-openstack-amd64 \
+  --size 10 \
+  --type standard \
+  debian-10-openstack-amd64
