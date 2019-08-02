e6dc723bd940fc77f45038ad3ed65c4174bfb4e4
Valdemar Lemche <valdemar@lemche.net>
2019-08-02 09:08:57 +0200

    added script to ensure that packages allowing https traffic (i.e. when downloading stuff) is available

---
+++ openstack/100-prerequisites/102-install-preprequisites.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+##############################################################################
+# Install packages prequisite to installing prequisites
+##############################################################################
+sudo apt-get install \
+  openssl \
+  ca-certificates \
+  ssl-cert
