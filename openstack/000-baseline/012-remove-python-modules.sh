37a6e480621055349ad21715e02c0abcb9d3ef1a
Valdemar Lemche <valdemar@lemche.net>
2019-08-02 09:05:06 +0200

    just fixed some formatting of shell commands

--- openstack/000-baseline/012-remove-python-modules.sh
+++ openstack/000-baseline/012-remove-python-modules.sh
@@ -5,5 +5,7 @@
 ##############################################################################
 sudo apt-get install --yes python-pip
 for i in $(pip list | awk '{print $1}'); do sudo -i pip uninstall -y $i; done
-sudo apt-get --reinstall --yes install \
+sudo apt-get install \
+  --reinstall \
+  --yes \
   $(echo $(dpkg -l | awk '{print $2}' | tail -n +5 | grep ^python-) | sed 's|\n| |g')
