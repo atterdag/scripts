743a1c5e58f7c748634a8f4005d60c5210882f9b
Valdemar Lemche <valdemar@lemche.net>
2020-01-05 17:34:53 +0100

    elaboraties on how to check if placement service is working

--- openstack/400-openstack-core/450-nova-controller-post.sh
+++ openstack/400-openstack-core/450-nova-controller-post.sh
@@ -33,4 +33,78 @@ sudo systemctl restart \
   nova-api \
   qemu-kvm
 
+# You might have to wait a few minutes before the compute provider is
+# registered with the placement service.
 sudo -E nova-status upgrade check
+
+# Not registered yet:
+# +-------------------------------------------------------------------+
+# | Upgrade Check Results                                             |
+# +-------------------------------------------------------------------+
+# | Check: Cells v2                                                   |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+# | Check: Placement API                                              |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+# | Check: Resource Providers                                         |
+# | Result: Warning                                                   |
+# | Details: There are no compute resource providers in the Placement |
+# |   service but there are 1 compute nodes in the deployment.        |
+# |   This means no compute nodes are reporting into the              |
+# |   Placement service and need to be upgraded and/or fixed.         |
+# |   See                                                             |
+# |   https://docs.openstack.org/nova/latest/user/placement.html      |
+# |   for more details.                                               |
+# +-------------------------------------------------------------------+
+# | Check: Ironic Flavor Migration                                    |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+# | Check: API Service Version                                        |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+# | Check: Request Spec Migration                                     |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+# | Check: Console Auths                                              |
+# | Result: Success                                                   |
+# | Details: None                                                     |
+# +-------------------------------------------------------------------+
+#
+# All done:
+# +--------------------------------+
+# | Upgrade Check Results          |
+# +--------------------------------+
+# | Check: Cells v2                |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: Placement API           |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: Resource Providers      |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: Ironic Flavor Migration |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: API Service Version     |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: Request Spec Migration  |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
+# | Check: Console Auths           |
+# | Result: Success                |
+# | Details: None                  |
+# +--------------------------------+
