8b34a356697e33560519990b6d5411b5581a1756
Valdemar Lemche <valdemar@lemche.net>
2020-01-05 17:36:17 +0100

    added some more examples to checking test1 server creation

---
+++ openstack/900-openstack-test-servers/802-test1-server-console-log.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+##############################################################################
+# Get URL for connecting to server instance on Controller host
+##############################################################################
+openstack console log show \
+  test1
