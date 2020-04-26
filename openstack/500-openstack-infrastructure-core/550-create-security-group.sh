042088f2e2e0069e8eb0c4ec3810e7d175376049
Valdemar Lemche <valdemar@lemche.net>
2020-04-26 10:15:04 +0200

    created own global default security group, that can be reused accross projects

--- openstack/500-openstack-infrastructure-core/550-create-security-group.sh
+++ openstack/500-openstack-infrastructure-core/550-create-security-group.sh
@@ -3,10 +3,27 @@
 ##############################################################################
 # Create default security on Controller host
 ##############################################################################
+# The default group is created for all projects, but that means that you
+# cannot just refer to it by its name, because there are multiple default
+# groups. So to workaround that, just create a global default group that will
+# have a unique name.
+openstack security group create \
+  --description "Global default rule" \
+  global_default
+openstack security group rule create \
+  --ingress \
+  --ethertype IPv4 \
+  --remote-ip "0.0.0.0/0" \
+  global_default
+openstack security group rule create \
+  --ingress \
+  --ethertype IPv6 \
+  --remote-ip "::/0" \
+  global_default
 openstack security group rule create \
   --proto icmp \
-  default
+  global_default
 openstack security group rule create \
   --proto tcp \
   --dst-port 22 \
-  default
+  global_default
