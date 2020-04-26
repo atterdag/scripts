e6e3edd99e19395dad51f34cb0513c05b04e4734
Valdemar Lemche <valdemar@lemche.net>
2020-04-26 10:16:12 +0200

    reviewed flavors to optimize CPU utilization

--- openstack/500-openstack-infrastructure-core/570-create-flavors.sh
+++ openstack/500-openstack-infrastructure-core/570-create-flavors.sh
@@ -7,29 +7,37 @@ openstack flavor create \
   --disk 1 \
   --public \
   --ram 128 \
-  --vcpus 2 \
+  --vcpus 1 \
+  --property hw:cpu_policy=shared \
+  --property hw:cpu_cores=1 \
+  --property hw:cpu_sockets=1 \
+  --property hw:cpu_threads=1 \
   m1.tiny
 openstack flavor create \
   --disk 5 \
   --public \
   --ram 256 \
-  --vcpus 2 \
+  --vcpus 1 \
+  --property hw:cpu_policy=shared \
   m1.small
 openstack flavor create \
   --disk 5 \
   --public \
   --ram 512 \
   --vcpus 2 \
+  --property hw:cpu_policy=shared \
   m1.medium
 openstack flavor create \
   --disk 5 \
   --public \
   --ram 1024 \
   --vcpus 2 \
+  --property hw:cpu_policy=shared \
   m1.large
 openstack flavor create \
   --disk 5 \
   --public \
   --ram 2048 \
-  --vcpus 2 \
+  --vcpus 4 \
+  --property hw:cpu_policy=shared \
   m1.huge
