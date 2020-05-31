02cbfb51d3383cfb8a10f100665494ecfd8c6d98
Valdemar Lemche <valdemar@lemche.net>
2020-06-01 01:26:23 +0200

    added pfsense image to glance

---
+++ openstack/500-openstack-infrastructure-core/566-create-pfsense-image.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+##############################################################################
+# Create ubuntu-18.04-server-cloudimg-amd64 images on Controller host
+##############################################################################
+# Ref https://docs.openstack.org/image-guide/obtain-images.html
+sudo wget \
+  --continue \
+  --output-document=${OPENSTACK_IMAGES_DIRECTORY}/pfSense-CE-memstick-2.4.5-RELEASE-amd64.img.gz \
+  https://frafiles.pfsense.org/mirror/downloads/pfSense-CE-memstick-2.4.5-RELEASE-amd64.img.gz
+
+gunzip ${OPENSTACK_IMAGES_DIRECTORY}/pfSense-CE-memstick-2.4.5-RELEASE-amd64.img.gz
+
+openstack image create \
+  --container-format bare \
+  --disk-format raw \
+  --file ${OPENSTACK_IMAGES_DIRECTORY}/pfSense-CE-memstick-2.4.5-RELEASE-amd64.img \
+  --public \
+  pfSense-CE-memstick-2.4.5-RELEASE-amd64
