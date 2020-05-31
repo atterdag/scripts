025904f8732e12f9e827a7aedc45c204877785ba
Valdemar Lemche <valdemar@lemche.net>
2020-05-31 20:36:53 +0200

    added support to install etcd on arm64

--- openstack/100-prerequisites/110-install-etcd-management.sh
+++ openstack/100-prerequisites/110-install-etcd-management.sh
@@ -4,7 +4,27 @@
 # Install Etcd on Controller host
 ##############################################################################
 sudo add-apt-repository --enable-source --yes --update universe
+
+if [[ $(uname --machine) == aarch64 ]]; then
+  cat <<EOF | sudo tee /usr/sbin/policy-rc.d
+#!/bin/sh
+exit 101
+EOF
+  sudo chmod +x /usr/sbin/policy-rc.d
+fi
+
 sudo apt-get --yes --quiet install \
   etcd
+
+if [[ $(uname --machine) == aarch64 ]]; then
+  if [[ -f /usr/sbin/policy-rc.d ]]; then
+    sudo rm -f /usr/sbin/policy-rc.d
+  fi
+fi
+
+if [[ $(uname --machine) == aarch64 ]]; then
+  echo 'ETCD_UNSUPPORTED_ARCH=arm64' | sudo tee -a /etc/default/etcd
+fi
+
 sudo systemctl enable etcd
 sudo systemctl start etcd
