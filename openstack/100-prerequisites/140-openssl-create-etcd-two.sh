3f946e724bbfebe41bef068765c40431526f6410
Valdemar Lemche <valdemar@lemche.net>
2020-06-06 08:16:03 +0200

    Changed order of execution of scripts to improve flow of deployment

--- openstack/100-prerequisites/140-openssl-create-etcd-two.sh
+++ openstack/100-prerequisites/140-openssl-create-etcd-two.sh
@@ -15,7 +15,7 @@ fi
 sudo su -c "openssl req \
   -batch \
   -config <(cat ${SSL_BASE_DIR}/${SSL_INTERMEDIATE_CA_ONE_STRICT_NAME}/openssl.cnf; \
-    printf \"[SAN]\nsubjectAltName=DNS:${ETCD_TWO_FQDN},IP:${ETCD_ONE_IP_ADDRESS}\") \
+    printf \"[SAN]\nsubjectAltName=DNS:${ETCD_TWO_FQDN},IP:${ETCD_TWO_IP_ADDRESS}\") \
   -keyout ${SSL_BASE_DIR}/${SSL_INTERMEDIATE_CA_ONE_STRICT_NAME}/private/${ETCD_TWO_FQDN}.key \
   -new \
   -newkey rsa:2048 \
