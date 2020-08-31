63d34ef4d4be2d49485a63a48080aaf58acdad7e
Valdemar Lemche <valdemar@lemche.net>
2020-08-31 21:39:41 +0200

    added clean up of docker packages on hosts

--- openstack/kolla/10-destroy-kolla-env.sh
+++ openstack/kolla/10-destroy-kolla-env.sh
@@ -48,3 +48,26 @@ echo '***'
 for mount_point in $(df -T|grep -E "ext4|btrfs"|awk '{print $7}'); do
   sudo fstrim -v $mount_point
 done
+
+echo '***'
+echo '*** delete kolla user'
+echo '***'
+sudo userdel -r kolla
+
+echo '***'
+echo '*** remove docker packages'
+echo '***'
+sudo /etc/init.d/docker stop
+sudo apt-get --yes --quiet --reinstall remove \
+  docker-ce
+sudo apt-get --yes --quiet --purge autoremove
+sudo rm -f /etc/apt/sources.list.d/docker.list
+sudo apt-key del 0EBFCD88
+
+echo '***'
+echo '*** removing docker space'
+echo '***'
+sudo umount /var/lib/docker
+sudo sed -i '/system-docker/,$d' /etc/fstab
+sudo rmdir /var/lib/docker
+sudo lvremove --yes system/docker
